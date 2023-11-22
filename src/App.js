393325f3c921e8ed3a11a496a7dee7617d1dfd4e
iamsarvagya1296 <iamsarvagya.saxena@gmail.com>
2023-11-22 12:55:56 +0530

    build pushing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Temp from './components/weather/temp'; 
+
+function App() {
+  return (
+   <>
+    <div>
+
+      <Temp />
+    </div>
+   </>
+  );
+}
+
+export default App;
